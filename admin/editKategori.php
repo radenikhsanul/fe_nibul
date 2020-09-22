6b5a710c5c85b9e86ed6fd5f9e37a7f92c7d072f
user <user@gmail.com>
2020-09-22 15:07:54 +0700

    update kategori

---
+++ admin/editKategori.php
@@ -0,0 +1,46 @@
+<?php 
+include 'header.php';
+?>
+
+<?php include("config.php"); ?>
+
+
+
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                 <div class="row">
+                    <div class="col-md-6">
+                     <h2>Edit Kategori</h2>   
+                     
+                    </div>
+                </div>             
+                 <!-- /. ROW  -->
+                  <hr />
+				<form action="#" method="post">
+					<div class="form-group">
+						<label>Nama Kategori</label>
+						<input name="nama" type="text" class="form-control" placeholder="Nama Kategori" value="">
+					</div>													
+
+				</div>
+					<button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+					<input type="submit" class="btn btn-primary" value="Simpan" name="simpan">
+			</form>
+		</div>
+	</div>
+</div>
+
+
+                   
+                 <!-- /. ROW  -->           
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+<?php 
+
+
+include 'footer.php';
+?>
